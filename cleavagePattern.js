1c71065068ac6e630ca2f757e975eb4bd7daed5c
Rohan Palankar <rohanpalankar8@gmail.com>
2025-08-26 14:34:10 +0530

    cleavage pattern of numbers added

--- cleavagePattern.js
+++ cleavagePattern.js
@@ -1,26 +1,47 @@
 function cleavagePattern(n) {
   let space = n * 2 - 3;
-  let stars = 1;
   let pattern = "";
 
   for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= stars; j++) {
+    for (let j = 1; j <= i; j++) {
       pattern += "*";
     }
     for (let k = 1; k <= space; k++) {
       pattern += " ";
     }
-    for (let l = 1; l <= stars; l++) {
+    for (let l = 1; l <= i; l++) {
       if (l < n) {
         pattern += "*";
       }
     }
     pattern += "\n";
     space -= 2;
-    stars += 1;
   }
 
   return pattern;
 }
 
-console.log(cleavagePattern(12));
+console.log(cleavagePattern(5));
+
+function cleavagePatternOfNums(n) {
+  let space = 2 * n - 3;
+  let pattern = "";
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      pattern += j;
+    }
+    for (let k = 1; k <= space; k++) {
+      pattern += " ";
+    }
+    for (let j = i; j >= 1; j--) {
+      if (j < n) pattern += j;
+    }
+    pattern += "\n";
+    space -= 2;
+  }
+
+  return pattern;
+}
+
+console.log(cleavagePatternOfNums(5));

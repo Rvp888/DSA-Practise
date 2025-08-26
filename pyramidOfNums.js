4299820f139317f93ae13e14c8dd6c4fd77d7419
Rohan Palankar <rohanpalankar8@gmail.com>
2025-08-26 10:22:37 +0530

    pyramid of numbers pattern problem updated for binary version

--- pyramidOfNums.js
+++ pyramidOfNums.js
@@ -19,3 +19,25 @@ function pyramidPattern(n) {
 }
 
 console.log(pyramidPattern(5));
+
+function binaryPyramidPattern(n) {
+  let space = n - 1;
+  let nums = 1;
+  let pattern = "";
+
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= space; j++) {
+      pattern += " ";
+    }
+    for (k = 1; k <= nums; k++) {
+      pattern += k % 2;
+    }
+    pattern += "\n";
+    space -= 1;
+    nums += 2;
+  }
+
+  return pattern;
+}
+
+console.log(binaryPyramidPattern(5));
